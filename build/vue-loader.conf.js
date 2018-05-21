99f0f57e078385990446c22ece0a689630c69bc2
李梦龙 <649076408@qq.com>
2018-05-21 09:24:28 +0800

    docs($site): config vuepress

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,12 +1,13 @@
 var utils = require('./utils')
-var config = require('../config')
-var isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  loaders: utils.cssLoaders({
-    sourceMap: isProduction
-      ? config.build.productionSourceMap
-      : config.dev.cssSourceMap,
-    extract: isProduction
-  })
+  loaders: {
+    ...utils.cssLoaders({
+      sourceMap: true,
+      extract: process.env.NODE_ENV === 'production'
+    }),
+    // Module build failed: TypeError: Cannot read property 'transforms' of null
+    // 兼容 vuepress 打包时候报错
+    js: "babel-loader",
+  }
 }
--- site/views/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="home">
-    <router-link :to="{ name: 'Icon'}">组件</router-link>
-    <a href="https://github.com/fe6">源码</a>
-  </div>
-</template>
-<style lang="scss">
-@import '../assets/styles/home.scss';
-</style>
